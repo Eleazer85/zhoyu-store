e23b15ab6bc5ada196cbe36f418b789148726045
Eleazer85 <audwineleazer@gmail.com>
2025-03-04 12:07:08 +0700

    changing the Games tables structure (adding new column description)

--- web-top-up.sql
+++ web-top-up.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Feb 26, 2025 at 05:49 AM
+-- Generation Time: Mar 04, 2025 at 06:03 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -48,6 +48,7 @@ INSERT INTO `Gambar-thumbnail` (`Gambar`, `Game_terkait`) VALUES
 CREATE TABLE `Games` (
   `ID` varchar(255) NOT NULL,
   `Nama-game` varchar(3000) NOT NULL,
+  `Description` text DEFAULT NULL,
   `Gambar-game` varchar(3000) NOT NULL,
   `Likes` int(255) NOT NULL DEFAULT 0,
   `Game_terkait` varchar(255) DEFAULT NULL,
@@ -58,19 +59,19 @@ CREATE TABLE `Games` (
 -- Dumping data for table `Games`
 --
 
-INSERT INTO `Games` (`ID`, `Nama-game`, `Gambar-game`, `Likes`, `Game_terkait`, `date_added`) VALUES
-('010-00001', 'Point Blank', 'Images/PointBlank.jpg', 0, 'point-blank', '2025-02-25 07:16:41'),
-('100-00001', 'Mobile Legends', 'Images/Mobile-legend.jpeg', 0, 'mobile-legends', '2025-02-24 17:00:00'),
-('100-00002', 'PUBG', 'Images/PUBG.jpg', 0, 'pubg', '2025-02-24 17:00:00'),
-('100-00003', 'Valorant', 'Images/Valorant.jpg', 0, 'valorant', '2025-02-24 17:00:00'),
-('100-00004', 'Call of Duty Mobile', 'Images/Cod-Mobile.png', 0, 'call-of-duty-mobile', '2025-02-24 17:00:00'),
-('100-00005', 'Free Fire', 'Images/Free-Fire.png', 0, 'free-fire', '2025-02-24 17:00:00'),
-('100-00006', 'Arena Breakout', 'Images/Arena-Breakout.png', 0, 'arena-breakout', '2025-02-24 17:00:00'),
-('111-00001', 'Genshin Impact', 'Images/Genshin-Impact.jpg', 0, 'genshin-impact', '2025-02-24 17:00:00'),
-('111-00002', 'Roblox', 'Images/roblox.png', 0, 'roblox', '2025-02-24 17:00:00'),
-('111-00003', 'Steam Wallet', 'Images/Steamwallet.jpg', 0, 'steam-wallet', '2025-02-24 17:00:00'),
-('111-00004', 'Honkai Star Rail', 'Images/HonkaiStarRail.jpg', 0, 'honkai-star-rail', '2025-02-24 17:00:00'),
-('111-00005', 'Zenless Zone Zero (ZZZ)', 'Images/ZZZ.jpg', 0, 'zenless-zone-zero', '2025-02-24 17:00:00');
+INSERT INTO `Games` (`ID`, `Nama-game`, `Description`, `Gambar-game`, `Likes`, `Game_terkait`, `date_added`) VALUES
+('010-00001', 'Point Blank', NULL, 'Images/PointBlank.jpg', 0, 'point-blank', '2025-02-25 07:16:41'),
+('100-00001', 'Mobile Legends', NULL, 'Images/Mobile-legend.jpeg', 0, 'mobile-legends', '2025-02-24 17:00:00'),
+('100-00002', 'PUBG', NULL, 'Images/PUBG.jpg', 0, 'pubg', '2025-02-24 17:00:00'),
+('100-00003', 'Valorant', NULL, 'Images/Valorant.jpg', 0, 'valorant', '2025-02-24 17:00:00'),
+('100-00004', 'Call of Duty Mobile', NULL, 'Images/Cod-Mobile.png', 0, 'call-of-duty-mobile', '2025-02-24 17:00:00'),
+('100-00005', 'Free Fire', NULL, 'Images/Free-Fire.png', 0, 'free-fire', '2025-02-24 17:00:00'),
+('100-00006', 'Arena Breakout', NULL, 'Images/Arena-Breakout.png', 0, 'arena-breakout', '2025-02-24 17:00:00'),
+('111-00001', 'Genshin Impact', NULL, 'Images/Genshin-Impact.jpg', 0, 'genshin-impact', '2025-02-24 17:00:00'),
+('111-00002', 'Roblox', NULL, 'Images/roblox.png', 0, 'roblox', '2025-02-24 17:00:00'),
+('111-00003', 'Steam Wallet', NULL, 'Images/Steamwallet.jpg', 0, 'steam-wallet', '2025-02-24 17:00:00'),
+('111-00004', 'Honkai Star Rail', NULL, 'Images/HonkaiStarRail.jpg', 0, 'honkai-star-rail', '2025-02-24 17:00:00'),
+('111-00005', 'Zenless Zone Zero (ZZZ)', NULL, 'Images/ZZZ.jpg', 0, 'zenless-zone-zero', '2025-02-24 17:00:00');
 
 -- --------------------------------------------------------
 
@@ -92,7 +93,8 @@ CREATE TABLE `Katalog` (
 --
 
 INSERT INTO `Katalog` (`Harga`, `Nominal`, `Curency`, `Gambar`, `Tipe`, `Game_terkait`) VALUES
-(1100, 3, 'Diamonds', 'Images/Game-money/2.png', 'Region-Indonesia', 'mobile-legend');
+(1100, 3, 'Diamonds', 'Images/Game-money/2.png', 'Region-Indonesia', 'mobile-legends'),
+(1600, 5, 'Diamonds', 'Images/Game-money/2.png', 'Region-Indonesia', 'mobile-legends');
 
 --
 -- Indexes for dumped tables
@@ -102,7 +104,8 @@ INSERT INTO `Katalog` (`Harga`, `Nominal`, `Curency`, `Gambar`, `Tipe`, `Game_te
 -- Indexes for table `Games`
 --
 ALTER TABLE `Games`
-  ADD PRIMARY KEY (`ID`);
+  ADD PRIMARY KEY (`ID`),
+  ADD UNIQUE KEY `Game_terkait` (`Game_terkait`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
